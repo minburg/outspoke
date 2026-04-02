a6d54211b4a85a3b0fdff0fe570baf7715272cd7
Marinus <marinus.burger@gmail.com>
2026-04-02 17:45:42 +0200

    add line-breaks in full_description.txt and dependenciesInfo to build.gradle.kts to disable dependency metadata when building APKs/AABs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,12 @@ android {
             isUniversalApk = true
         }
     }
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs (for IzzyOnDroid/F-Droid)
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles (for Google Play)
+        includeInBundle = false
+    }
 }
 
 kotlin {
